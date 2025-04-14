0eacf0c907ff29ac0683dc0339ec14a8543759ff
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-14 18:04:39 +0200

    changes for use enviroments properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environments';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pp';
+
+  constructor() {
+    console.log(environment.apiKey);
+  }
+
 }
