4b2f7462ff492fb31f3d84bc8009b5fe0092180f
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-18 13:13:05 +0200

    modify communities.ts

--- src/app/utils/communities.ts
+++ src/app/utils/communities.ts
@@ -1,3 +1,5 @@
+import { weatherLocateTime } from "./interfaces";
+
 export const comunidadesAutonomas: string[] = [
   "Andalucía",
   "Aragón",
@@ -20,6 +22,143 @@ export const comunidadesAutonomas: string[] = [
   "Melilla"
 ];
 
+export const comunidadesResponse: weatherLocateTime[] = [
+  {
+    "locate": "Andalucía",
+    "datetime": "2025-04-17",
+    "temp": 22,
+    "conditions": "Rain, Partially cloudy",
+    "icon": "rain"
+  },
+  {
+    "locate": "Aragón",
+    "datetime": "2025-04-17",
+    "temp": 18,
+    "conditions": "Clear",
+    "icon": "clear-day"
+  },
+  {
+    "locate": "Asturias",
+    "datetime": "2025-04-17",
+    "temp": 15,
+    "conditions": "Overcast",
+    "icon": "cloudy"
+  },
+  {
+    "locate": "Islas Baleares",
+    "datetime": "2025-04-17",
+    "temp": 20,
+    "conditions": "Partly cloudy",
+    "icon": "partly-cloudy-day"
+  },
+  {
+    "locate": "Canarias",
+    "datetime": "2025-04-17",
+    "temp": 24,
+    "conditions": "Sunny",
+    "icon": "clear-day"
+  },
+  {
+    "locate": "Cantabria",
+    "datetime": "2025-04-17",
+    "temp": 16,
+    "conditions": "Rain",
+    "icon": "rain"
+  },
+  {
+    "locate": "Castilla-La Mancha",
+    "datetime": "2025-04-17",
+    "temp": 21,
+    "conditions": "Mostly sunny",
+    "icon": "partly-cloudy-day"
+  },
+  {
+    "locate": "Castilla y León",
+    "datetime": "2025-04-17",
+    "temp": 17,
+    "conditions": "Fog",
+    "icon": "fog"
+  },
+  {
+    "locate": "Cataluña",
+    "datetime": "2025-04-17",
+    "temp": 19,
+    "conditions": "Rain showers",
+    "icon": "rain"
+  },
+  {
+    "locate": "Comunidad Valenciana",
+    "datetime": "2025-04-17",
+    "temp": 23,
+    "conditions": "Partly cloudy",
+    "icon": "partly-cloudy-day"
+  },
+  {
+    "locate": "Extremadura",
+    "datetime": "2025-04-17",
+    "temp": 20,
+    "conditions": "Sunny",
+    "icon": "clear-day"
+  },
+  {
+    "locate": "Galicia",
+    "datetime": "2025-04-17",
+    "temp": 14,
+    "conditions": "Rain",
+    "icon": "rain"
+  },
+  {
+    "locate": "Madrid",
+    "datetime": "2025-04-17",
+    "temp": 22,
+    "conditions": "Mostly clear",
+    "icon": "clear-day"
+  },
+  {
+    "locate": "Región de Murcia",
+    "datetime": "2025-04-17",
+    "temp": 25,
+    "conditions": "Sunny",
+    "icon": "clear-day"
+  },
+  {
+    "locate": "Navarra",
+    "datetime": "2025-04-17",
+    "temp": 18,
+    "conditions": "Cloudy",
+    "icon": "cloudy"
+  },
+  {
+    "locate": "País Vasco",
+    "datetime": "2025-04-17",
+    "temp": 16,
+    "conditions": "Rain",
+    "icon": "rain"
+  },
+  {
+    "locate": "La Rioja",
+    "datetime": "2025-04-17",
+    "temp": 17,
+    "conditions": "Partly cloudy",
+    "icon": "partly-cloudy-day"
+  },
+  {
+    "locate": "Ceuta",
+    "datetime": "2025-04-17",
+    "temp": 21,
+    "conditions": "Sunny",
+    "icon": "clear-day"
+  },
+  {
+    "locate": "Melilla",
+    "datetime": "2025-04-17",
+    "temp": 22,
+    "conditions": "Sunny",
+    "icon": "clear-day"
+  }
+]
+
+
 /**
  * Devolver una lista con todas las comunidades que contienen la cadena pasada.
  * 
@@ -27,6 +166,8 @@ export const comunidadesAutonomas: string[] = [
  * @returns {string[]|undefined} Si `text` se encuentra dentro de al menos una comunidad devolvera una array de al menos el nombre de una comunidad, `string[]`, sino devolvera `undefined`, indicando que no esta en ninguna comunidad.
  */
 export function findLocate(text: string) : string[] | undefined {
+  if (text.length == 0)
+    return ([...comunidadesAutonomas]);
   const findLoc = comunidadesAutonomas.filter((loc) => {
     if (loc.toLowerCase().includes(text.toLowerCase()))
       return loc;
@@ -35,14 +176,4 @@ export function findLocate(text: string) : string[] | undefined {
   if (findLoc.length == 0)
     return undefined;
   return findLoc;
-}
-
-/**
- * Transforma una cadena de texto, cambiando sus espacios ' ' a '-', para poder usarlo en la API.
- * 
- * @param {string} text El texto que quieres convertir.
- * @return {string} El texto convertido.
- */
-export function transformStringToApi (text: string) : string {
-  return text.replace(' ', '-');
 }
