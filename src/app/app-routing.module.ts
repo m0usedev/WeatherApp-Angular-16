c171cc22d8c41ca28b055acf06a3c8ba2b83e185
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-18 13:16:28 +0200

    changes app routing, module, html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherListComponent } from './components/weather-list/weather-list.component';
+import { WeatherLocatedComponent } from './components/weather-located/weather-located.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+	{ path: '', component: WeatherListComponent },
+	{ path: ':locate', component: WeatherLocatedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavWeatherComponent } from './components/nav-weather/nav-weather.component';
+import { WeatherListComponent } from './components/weather-list/weather-list.component';
+import { WeatherLocatedComponent } from './components/weather-located/weather-located.component';
+import { WeatherCardResumeComponent } from './components/weather-card-resume/weather-card-resume.component';
+import { WeatherCardInfoComponent } from './components/weather-card-info/weather-card-info.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavWeatherComponent,
+    WeatherListComponent,
+    WeatherLocatedComponent,
+    WeatherCardResumeComponent,
+    WeatherCardInfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
